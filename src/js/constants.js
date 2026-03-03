24e028f2cada9a5079d18bd18964c49c574c9889
Viki <hi@viki.moe>
2026-03-03 16:50:10 +0800

    chore: release version

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,5 @@
-export const VERSION = '1.3.7'
-export const UPDATED_AT = '2026-03-03T07:55:43.340Z'
+export const VERSION = '1.4.0'
+export const UPDATED_AT = '2026-03-03T08:50:10.818Z'
 export const STORAGE_KEY = 'r2-manager-config'
 export const THEME_KEY = 'r2-manager-theme'
 export const LANG_KEY = 'r2-manager-lang'
